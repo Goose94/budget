3aed49c55a2ccad0762be392fc31d270e541c8ad
Goose94 <james@fractal-labs.com>
2019-06-04 12:36:16 +0100

    email login complete

--- App.js
+++ App.js
@@ -1,54 +1,83 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { Platform, StyleSheet, Image, Text, View, Button } from 'react-native';
 
 import Header from './src/components/Header';
-import LoginForm from './src/components/LoginForm';
 
 import firebase from '@react-native-firebase/app';
 import auth from '@react-native-firebase/auth';
 
-type Props = {};
+import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
 
-export default class App extends Component<Props> {
+import Loading from './src/components/Loading';
+import SignUp from './src/components/SignUp';
+import Login from './src/components/Login';
+import Main from './src/components/Main';
 
-  state = { loggedIn: null };
+// type Props = {};
 
-  renderComponent() {
-    if (this.state.loggedIn) {
-      return (<Button
-        title="Sign out"
-        onPress={() => firebase.auth().signOut()} />)
-    } else {
-      return (
-        <LoginForm />
-      )
-    }
+const AppStack = createStackNavigator({Main:Main})
+const AuthStack = createStackNavigator({SignUp: SignUp, Login: Login})
+
+const AppNavigator = createSwitchNavigator(
+  {
+    Loading: Loading,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'Loading',
   }
+)
+
+class App extends React.Component {
   render() {
-    return (
-      <View>
-        <Header title='Budget' />
-        {this.renderComponent()}
-      </View>
-    );
+    return <AppNavigator />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default createAppContainer(AppNavigator);
+
+//
+// export default class App extends Component<Props> {
+//
+//   state = { loggedIn: null };
+//
+//   renderComponent() {
+//     if (this.state.loggedIn) {
+//       return (<Button
+//         title="Sign out"
+//         onPress={() => firebase.auth().signOut()} />)
+//     } else {
+//       return (
+//         <LoginForm />
+//       )
+//     }
+//   }
+//   render() {
+//     return (
+//       <View>
+//         <Header title='Budget' />
+//         {this.renderComponent()}
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
